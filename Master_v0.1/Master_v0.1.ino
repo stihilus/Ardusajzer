782a5027da4079940784f67eae66101afa41fb07
stihilus <stihilus@gmail.com>
2018-06-04 21:27:21 +0200

    Master and Melody BIG update

--- Master_v0.1/Master_v0.1.ino
+++ Master_v0.1/Master_v0.1.ino
@@ -1,55 +1,83 @@
-                                                    //Arduino Synth v0.1
-                                                    //------------------
-                                                    //            MASTER
-                                                    //------------------
+#include <Wire.h> // Biblioteka za i2c komunikaciju
+#include <LCD5110_Graph.h> // Biblioteka za LCD
 
-const int potButton = 2;                            // Potenciometer Button - Pin D2
-                                                    // Ako je pritisnuto dugme potButton 
-                                                    // ocitava se vrednost potenciometra
-const int tempoMinus = 3;                           // tempoMinus usporava tempo - Pin D3
-const int tempoPlus = 4;                            // tempoPlus ubrzava tempo - Pin D4
-const int tempoReset = 5;                           // retetTempo resetuje na 500ms - Pin D3
-int tempo = 250;                                    // Start Tempo u ms
+LCD5110 myGLCD(8,9,10,12,11); 
+
+// LCD PIN------Arduino PIN
+// 1 RST        D12
+// 2 CE         D11
+// 3 DC         D10
+// 4 DIN        D9
+// 5 CLK        D8
+// 6 VCC        5V - 3.3V
+// 7 LIGHT      GND
+// 8 GND        GND
+
+extern unsigned char SmallFont[]; // Odabir fonta
+
+const int pot = 2; // Dugme koje propusta vrednost potenciometra
+const int ledPin = 13; // signalni LED / Tempo LED
+const int tempoMinus = 3; // Usporavanje tempa
+const int tempoPlus = 4; // Ubrzavanje tempa
+const int tempoReset = 5; // Resetovanje tempa
+int tempo = 140;
 int brojac = 1;
-
-#include <Wire.h>                                   //ukljucivanje biblioteke Wire.h
+int trajanjeLed = 100;
+int stanjeLed = false;
+unsigned long trenutak = 0;
 
 void setup()
 {
-  pinMode(A3, INPUT);
-  pinMode(potButton, INPUT);
-  pinMode(tempoPlus, INPUT);
-  pinMode(tempoMinus, INPUT);
-  pinMode(tempoReset, INPUT);
+  Wire.begin(); // Otvanje i2c veze 
+  
+  myGLCD.InitLCD(); // Aktiviranje LCD
+  myGLCD.setFont(SmallFont); // Setovanje fonta
+  
   Serial.begin(9600);
-  Wire.begin();                                     // Otvaranje i2c komunikacije A4(SDA),A5(SCL)
+  
+  pinMode(A3,INPUT);
+  pinMode(pot,INPUT_PULLUP);
+  pinMode(tempoMinus,INPUT_PULLUP);
+  pinMode(tempoPlus,INPUT_PULLUP);
+  pinMode(tempoReset,INPUT_PULLUP);
+  pinMode(ledPin,OUTPUT);
 }
 
-void loop()
+void loop() // Slanje 1 2 3 4 "char"-a sa mastera
 {
-  Wire.beginTransmission(5);                        // pocetak i2c komunikacija preko 5 kanala
   
+  Wire.beginTransmission(5); // Salje samo 1
   if(brojac == 1)
   {
-    Wire.write('1');                                // slanje karaktera slave arduinu
+    Wire.write('1');
+    Serial.println("Poslato 1.");
+    treptanje();
   }
-  else if(brojac == 2)
+  else if(brojac == 2) // Salje i 1 i 2
   {
     Wire.write('1');
     Wire.write('2');
+    Serial.println("Poslato 1 i 2.");
+    treptanje();
   }
   else if(brojac == 3)
   {
     Wire.write('1');
     Wire.write('3');
+    Serial.println("Poslato 1 i 3.");
+    treptanje();
   }
   else if(brojac == 4)
   {
     Wire.write('1');
     Wire.write('4');
+    Serial.println("Poslato 1 i 4.");
+    treptanje();
   }
-
-  if(brojac<=3)
+  
+  Wire.endTransmission(); // Prekid i2c komunikacije
+  
+  if(brojac <= 3) // Brojac 
   {
     brojac = brojac + 1;
   }
@@ -57,34 +85,84 @@ void loop()
   {
     brojac = 1;
   }
-    
-  Wire.endTransmission();                           // kraj komunikacije
+  
+  Serial.print("Brojac je: "); Serial.println(brojac);
 
-                                                    // Provera da li je potButton HIGH ako jeste 
-                                                    // ocitava vrednost potenciometra povezanog na A3
-  if(digitalRead(potButton) == HIGH)
+  // Stampanje ardusajzer logo-a na LCD-u
+  myGLCD.clrScr();
+  myGLCD.print("ARDUSAJZER", 12, 15);
+  myGLCD.print("RULES!", 25, 30);
+  myGLCD.update();
+  
+  if(digitalRead(pot) == LOW) // Ako je pot dugme pritisnuto propustaje vrednost i setuj tempo
   {
-    tempo = map(analogRead(A3),0,1023,250,750);
+    tempo = map(analogRead(A3),0,1023,80,240);
+    myGLCD.clrScr();
+    myGLCD.print(String(tempo), 32, 20);
+    myGLCD.update();
+  }
+  else if(digitalRead(tempoMinus) == LOW) // ako je minus dugme pritisnuto skida tempo
+  {
+    if(tempo>=85)
+    {
+      tempo = tempo - 5;
+      myGLCD.clrScr();
+      myGLCD.print(String(tempo), 32, 20);
+      myGLCD.update();
+    }
+    else
+    {
+      tempo = 80;
+      myGLCD.clrScr();
+      myGLCD.print("min BPM", 22, 20);
+      myGLCD.update();
+      Serial.println("min BPM");
+    }
+  }
+  else if(digitalRead(tempoPlus) == LOW)
+  {
+    if(tempo<=235)
+    {
+      tempo = tempo + 5;
+      myGLCD.clrScr();
+      myGLCD.print(String(tempo), 32, 20);
+      myGLCD.update();
+    }
+    else
+    {
+      tempo = 240;
+      myGLCD.clrScr();
+      myGLCD.print("max BPM", 22, 20);
+      myGLCD.update();
+      Serial.println("max BPM");
+    }
   } 
-
-  // TEMPO MINUS
-  else if(digitalRead(tempoMinus) == HIGH)
+  else if(digitalRead(tempoReset) == LOW)
   {
-    tempo = tempo - 50;
-  } 
-
-  // TEMPO PLUS
-    else if(digitalRead(tempoPlus) == HIGH)
-  {
-    tempo = tempo + 50;
-  } 
-
-  // TEMPO RESET
-    else if(digitalRead(tempoReset) == HIGH)
-  {
-    tempo = 500;
+    tempo = 140;
+    myGLCD.clrScr();
+    myGLCD.print(String(tempo), 32, 20);
+    myGLCD.update();
   } 
   
-  delay(tempo);                                     // DELAY u zavistnosti od tempa
-  Serial.println(tempo);
+  delay(60000/tempo); // TEMPO
+  Serial.print(tempo); Serial.println(" BMP");
+}
+
+void treptanje() // Indikator tempa LED
+{
+  if((millis() - trenutak)>=trajanjeLed)
+  {
+    trenutak = millis();
+    if(stanjeLed==LOW)
+  {
+    stanjeLed = HIGH;
+
+  }
+  else
+  {
+    stanjeLed=LOW;
+  }
+  digitalWrite(ledPin,stanjeLed);
+  }
 }
--- Melody_v0.1/Melody_v0.1.ino
+++ Melody_v0.1/Melody_v0.1.ino
@@ -1,76 +1,130 @@
-                                                    //Arduino Synth v0.1
-                                                    //------------------
-                                                    //            MELODY
-                                                    //------------------
+#include <Wire.h>
 
-
-#include <Wire.h>                                   // inkludovanje Wire.h biblioteke
 const int tempoPin = 0;
+const int brojPinova = 8;
+const int brojTonova = 8;
+const int tonePin = 11;
+const int freqPins[brojPinova] = {2,3,5,12,7,8,9,10};
 const int ledPin = 13;
-const int freqPins[8] = {1,2,3,4,5,6,7,8};
-int tonePin = 9;
-int toneFrequency[8] = {200,0,0,0,0,0,0,0};
-int toneDuration[8] = {500,0,0,0,0,0,0,0};
-int ledLife = 100;
-int brojac = 1;
+int toneFrequency[brojTonova] = {450,500,550,600,650,700,750,800};
+int toneDuration[brojTonova] = {200,200,200,200,200,200,200,200};
+int brojacTempo = 1;
+int brojacTone = 0;
+int stanjeDugme[brojTonova] = {false,false,false,false,false,false,false,false};
+int opcija2 = 1000;
+unsigned long trenutak = 0;
+int duziStisak[brojTonova] = {false,false,false,false,false,false,false,false};
+int sviraNotu[brojTonova] = {true,true,true,true,true,true,true,true};
 
 void setup()
 {
-  pinMode(A1,INPUT);
-  pinMode(A2,INPUT);
-  for(int i = 0; i < 8; i++)
-  {
-    pinMode(freqPins[i],INPUT);
-  }
-  Wire.begin(5);                                    // otvaranje/slusanje i2c komunikacije preko 5 kanala
+  Wire.begin(5);
   Wire.onReceive(receiveEvent);
-  pinMode(ledPin, OUTPUT);                          // LED kao indikator TEMPA - Pin D13
+  pinMode(A3,INPUT);
+  pinMode(A2, INPUT);
+  pinMode(tempoPin,INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  for(int i = 0; i<brojPinova; i++)
+  {
+    pinMode(freqPins[i],INPUT_PULLUP);
+  }
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  if(digitalRead(tempoPin)==HIGH)
+  if(digitalRead(tempoPin)==LOW)
   {
-    if(brojac<=3)
+    if(brojacTempo<=3)
     {
-      brojac = brojac + 1;
+      brojacTempo = brojacTempo + 1;
+      //Serial.print("Brojac je: "); Serial.println(brojacTempo);
     }
     else
     {
-      brojac = 1;
+      brojacTempo = 1;
+      //Serial.print("Brojac je: "); Serial.println(brojacTempo);
     }
   }
-
-  for(int i = 0; i < 8; i++)
+  else
   {
-    if(digitalRead(i),HIGH)
+    for(int j = 0; j<brojPinova; j++)
     {
-      toneFrequency[i] = map(analogRead(A1),0,1023,15,7900);
-      toneDuration[i] = map(analogRead(A1),0,1023,46,4000);
-      Serial.print("Frekvencija za "); Serial.print(i); Serial.print(". zvuk je: "); Serial.println(toneFrequency[i]);
-      Serial.print("Trajanje za "); Serial.print(i); Serial.print(". zvuk je: "); Serial.println(toneDuration[i]);
+      if(digitalRead(freqPins[j])==LOW)
+      {
+        if(stanjeDugme[j] == false)
+        {
+          stanjeDugme[j] = true;
+          trenutak = millis();
+        }
+        if((millis()-trenutak>opcija2)&&(duziStisak[j] == false))
+        {
+          duziStisak[j] = true;
+          toneFrequency[j] = map(analogRead(A2),0,1023,15,7900);
+          toneDuration[j] = map(analogRead(A3),0,1023,46,4000);
+          Serial.print("Frekvencija za "); Serial.print(j+1); Serial.print(". zvuk je: "); Serial.println(toneFrequency[j]);
+          Serial.print("Trajanje za "); Serial.print(j+1); Serial.print(". zvuk je: "); Serial.println(toneDuration[j]);
+        }
+      }
+      else
+      {
+        if(stanjeDugme[j] == true)
+        {
+          if(duziStisak[j] == true)
+          {
+            duziStisak[j] = false;
+          }
+          else
+          {
+            sviraNotu[j] = !sviraNotu[j];
+            //Serial.print(j+1); Serial.println("Status note promenjen.");
+          }
+          stanjeDugme[j] = false;
+        }
+      }
     }
   }
+  delay(100);
+//  for(int i = 0;i<brojTonova;i++)
+//  {
+//    Serial.println(toneFrequency[i]);
+//  }
 }
 
-
-void receiveEvent(int howMany)                      //??? howMany
+void receiveEvent(int howMany)
 {
-  while(Wire.available())                           //Kad kod nesto stize preko i2c kanalom 5 uradi:
+  while(Wire.available())
   {
-    char c = Wire.read();                            //Promenljivoj C char se dodaljuje vrednosta sa i2c
+    
+    //Serial.println(brojacTempo);
+        
+    char c = Wire.read();
     int d = c - '0';
-   
-    if(d == brojac)
+    
+    //Serial.println("Primljen karakter "); Serial.println(c);
+    
+    if(d==brojacTempo)
     {
-      for(int i = 0; i < 8; i++)
+      if(brojacTone<8)
       {
-        tone(tonePin, toneFrequency[i], toneDuration[i]);    // Pusta se ton "tone(pin, frequency, duration)"
-        Serial.print("Svira zvuk "); Serial.println(i);
-        digitalWrite(ledPin, HIGH);                    // Pali se LED indikator
-        delay(ledLife);                                // Trajanje sveljenja diode
-        digitalWrite(ledPin, LOW);                     // Gasi se LED indikator
+        brojacTone = brojacTone + 1;
       }
+      else
+      {
+        brojacTone = 1;
+      }
+      if(sviraNotu[brojacTone-1]==true)
+      {
+        tone(tonePin, toneFrequency[brojacTone-1], toneDuration[brojacTone-1]);
+        //Serial.println("svira");
+        //Serial.println(brojacTone);
+        //Serial.println(toneFrequency[brojacTone-1]);
+        Serial.println(brojacTone);
+      }
+    }
+    else
+    {
+      digitalWrite(ledPin,LOW);
     }
   }
 }
